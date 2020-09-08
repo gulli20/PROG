10fc7ef468203a81537382822f83cfe73cdf2e34
gulli20 <gunnlaugur20@ru.is>
2020-09-08 21:57:07 +0000

    Program finished

--- Vika3/Project3/fibo_abundant.py
+++ Vika3/Project3/fibo_abundant.py
@@ -1,32 +1,18 @@
-print("Would you like to see fibonacci numbers, abundant numbers or both?")
-question_str = str(input("Input |f| for fibonacchi numbers, |a| for abundant numbers or both |b| for both: "))
+question_str = str(input("Input f|a|b (fibonacci, abundant or both): "))
 
-while_value = 0
-
-while while_value != 1:
-    if question_str != 'a' or question_str != 'f' or question_str != 'b':
-        print("I'm sorry, your input is invalid, please try again")
-    question_str = str(input("Input |f| for fibonacchi numbers, |a| for abundant numbers or both |b| for both: "))
-    if question_str == 'f' or question_str == 'a' or question_str == 'b':
-        while_value = 1
-
-
-print()
 # Fibonachi
-
-    # 1 - Program variables for the for loop
-    # 2 - Ask the user for the lenght of the fibonacci sequence the user wants
-    # 3 - Program a for loop that outputs the first n numbers in the fibonacci sequence where n is the input from the user
-    # 4 - The formula for the sequence is the sum of the 2 numbers before the next number is the next number in the sequence
+    # 1 - Ask the user the lenght 'n' of the fibonacci sequence.
+    # 2 - Program a for loop that outputs the first 'n' numbers in the fibonacci sequence.
+    # 3 - The formula for the fibonacci sequence is the sum of the 2 numbers before the next number is the next number in the sequence (num_one + num_two = next_num) and it always starts with 1, 2.
 
 if question_str == 'f' or question_str == 'b':
     
-    n_fibo_int = int(input("Input the lenght of the sequence: "))
+    n_fibo_int = int(input("Input the length of the sequence: "))
     num_one_int = 0
     num_two_int = 1
     num_next_int = 0
-    print("Here are the first", n_fibo_int, "numbers in the fibonacci sequence:")
-    print("_")
+    print("Fibonacci Sequence:")
+    print("-------------------")
     for i in range(0, n_fibo_int+1):
         # Print the first 2 numbers as they are always 0, 1
         if i < 2:
@@ -36,20 +22,18 @@ if question_str == 'f' or question_str == 'b':
             num_one_int = num_two_int
             num_two_int = num_next_int
             print(num_next_int)
-print()
 
 # Abundant numbers
-
-    # 1 - Find all the divisors of a number and sum them up
-    # 2 - If the sum of the divisors of a given number is larger than the number it self it is an abundant number
-    # 3 - Program a for loop that checks every number from 1 to n if it is an abundant number, where n is an input from the user
-    # 4 - Output all abundant numbers from 1 to n
+    # 1 - To find a abundant number: if the sum of all divisors (that leave no remainders) of a given number is larger than the number it is abundant
+    # 2 - Find all the divisors of a number and sum them up
+    # 3 - Program a for loop that checks every number from '1 to n', if it is an abundant number, where 'n' is an input from the user
+    # 4 - Output all abundant numbers from '1 to n'
 
 if question_str == 'a' or question_str == 'b':
 
-    n_abundant_int = int(input("Set the range to check for abundant numbers from 1 to: "))
-    print("Abundant numbers in the range of 1 to", n_abundant_int, ":")
-    print("_")
+    n_abundant_int = int(input("Input the max number to check: "))
+    print("Abundant numbers:")
+    print("-----------------")
     sum_divisor = 0
 
     for i in range(1, n_abundant_int+1):
@@ -60,10 +44,3 @@ if question_str == 'a' or question_str == 'b':
             print(i)
             sum_divisor = 0
         sum_divisor = 0
-
-print()
-restart_str = str(input("Would you like to go again? (y/n): "))
-
-if restart_str == 'n':
-    while_value = 0
-    continue
